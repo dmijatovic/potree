7e401cc328b15389876279b18d5ae04df04a0015
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2024-01-19 09:59:38 +0100

    feat: vpc copc loader with additional examples

--- src/loader/VpcLoader.js
+++ src/loader/VpcLoader.js
@@ -1,43 +1,68 @@
-import {VpcBaseGeometry} from '../PointCloudVpcGeometry';
-import {PointCloudCopcGeometryNode} from "../PointCloudCopcGeometry"
+import {PointCloudCopcGeometryNode,PointCloudCopcGeometry} from "../PointCloudCopcGeometry"
+import {PointCloudOctree} from "../PointCloudOctree.js";
 
-export class VpcLoader {
+/**
+ * Use CopcLoader when *.copc.laz file are used
+ * NOTE! We need to adjust the elevationRange based
+ * on values calculated for each file
+ */
+export class VpcLoader{
 	static async load(file, callback) {
-		const response = await fetch(file)
+		const {Copc,Getter} = window.Copc
+		const vpcPointclouds = []
+		let minRange, maxRange
 
+		// load vpc json file
+		const response = await fetch(file)
 		if(!response.ok) {
 			console.error(`Failed to load file form ${file}`);
 			callback(null);
 			return;
 		}
-
 		const vpc = await response.json();
-		const vpcGeometries = []
 
 		// start group logging
 		// console.group("VpcLoader.load")
-
-		// debugger
+		// load geometry data defined in vpc features
 		for (const feature of vpc.features){
-			const baseGeometry = new VpcBaseGeometry(vpc,feature);
+			const url = feature.assets.data.href;
+			const getter = Getter.http(url);
+			const copc = await Copc.create(getter);
 
-			// load propper copc getter for this file
-			if (await baseGeometry.loadGetter()===false){
-				throw Error("Failed to load getter in baseGeometry")
-			}
+			const copcGeometry = new PointCloudCopcGeometry(getter, copc);
 			// assign root tree node to base geometry class
-			baseGeometry.root = new PointCloudCopcGeometryNode(baseGeometry);
-			await baseGeometry.root.load();
+			copcGeometry.root = new PointCloudCopcGeometryNode(copcGeometry);
+			await copcGeometry.root.load();
 
-			// console.log("baseGeometry...", baseGeometry)
-			// add this geometry
-			vpcGeometries.push(baseGeometry)
+			// create pointcloud
+			const pointcloud = new PointCloudOctree(copcGeometry);
+			// use feature id as pointcloud name
+			pointcloud.name = feature.id
+
+			// calculate "overall" elevationRange
+			minRange = minRange === undefined ?
+				pointcloud.material.elevationRange[0] :
+				Math.min(pointcloud.material.elevationRange[0], minRange);
+			maxRange = maxRange === undefined ?
+				pointcloud.material.elevationRange[1] :
+				Math.max(pointcloud.material.elevationRange[1], minRange);
+
+			// debugger
+			// add vpcPointclouds
+			vpcPointclouds.push(pointcloud)
 		}
 
-		// console.log("vpcGemometries...", vpcGeometries)
+		// update elevationRange in all pointclouds
+		for (const pointcloud of vpcPointclouds) {
+			// debugger
+			pointcloud.material.elevationRange = [
+				minRange,
+				maxRange
+			]
+		}
+		// console.log("vpcGeometries...", vpcGeometries)
 		// console.groupEnd()
-
-		// return vpc geometry
-		callback(vpcGeometries);
+		// return vpc pointclouds
+		callback(vpcPointclouds);
 	}
 }
